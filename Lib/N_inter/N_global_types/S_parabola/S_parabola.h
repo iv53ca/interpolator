703fbb3e4641bafdb1379935b6f1c37293d5fc1c
Sophia Kaniukova <kaniukova.ss@phystech.edu>
2024-07-05 13:52:20 +0300

    S_parabola

---
+++ Lib/N_inter/N_global_types/S_parabola/S_parabola.h
@@ -0,0 +1,15 @@
+#pragma once
+// --------------------------------------------------------
+
+#include "N_inter/N_global_types/C_point.h"
+// --------------------------------------------------------
+
+namespace N_inter {
+    namespace n_global_types {
+        struct S_circle {
+            N_inter::n_global_types::C_point m_center;
+            double m_radius;
+        };
+    }
+}
+// --------------------------------------------------------
