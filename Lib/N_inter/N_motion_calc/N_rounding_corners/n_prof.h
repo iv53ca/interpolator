3b8d861d9fe4fd6fa81f559d4782a8b573a7df62
Sophia Kaniukova <kaniukova.ss@phystech.edu>
2024-07-05 13:26:02 +0300

    N_rounding_corners

---
+++ Lib/N_inter/N_motion_calc/N_rounding_corners/n_prof.h
@@ -0,0 +1,13 @@
+#pragma once
+// --------------------------------------------------------
+
+namespace N_inter {
+    namespace n_motion_calc {
+        namespace n_rounding_corners {
+            namespace n_prof {
+                // TODO Составить алгоритм и реализовать
+            }
+        }
+    }
+}
+// --------------------------------------------------------
